2494c546b17b5907c25fd5dced1fcdff680602e6
Valentin Boettcher <valentin@boettcher.cf>
2020-08-02 18:14:50 +0200

    correct syntax error

--- test/stream/commander_test.clj
+++ test/stream/commander_test.clj
@@ -220,8 +220,8 @@
       (testing "stopping the process"
         (let [prom (api/wait-for!
                     (:supervisor proc)
-                    :matcher (or (= (:event %1) :inactive)
-                                (= (:event %1) :failed)))]
+                    :matcher #(or (= (:event %1) :inactive)
+                                 (= (:event %1) :failed)))]
           (api/start-process! proc)
           (api/stop-process! proc)
           (is (not (= :timeout @prom)))
